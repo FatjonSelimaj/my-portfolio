fe867337a1e469ce7f93802e2a8dba8f4320a5f9
FatjonSelimaj <selimajfatjon16@gmail.com>
2025-05-21 13:58:21 +0200

    erro deploy

--- src/app/api/publicData/[id]/visits/route.ts
+++ src/app/api/publicData/[id]/visits/route.ts
@@ -3,14 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 const visitsStore: Record<string, number> = {};
 
-export async function GET(req: NextRequest, context: { params: { id: any; }; }) {
-  const { id } = context.params;
+export async function GET(req: NextRequest, context: any) {
+  const id = context.params.id;
   const count = visitsStore[id] ?? 0;
   return NextResponse.json({ visits: count });
 }
 
-export async function POST(req: NextRequest, context: { params: { id: any; }; }) {
-  const { id } = context.params;
+export async function POST(req: NextRequest, context: any) {
+  const id = context.params.id;
   visitsStore[id] = (visitsStore[id] ?? 0) + 1;
   return NextResponse.json({ visits: visitsStore[id] });
 }
